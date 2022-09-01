94409c2ed87d81054164a3f687d4f3b4e7049ec7
Wen Gong <gongwen1201@gmail.com>
2022-09-01 16:59:11 +1000

    mycoolwebsite

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,40 @@
+
+$(function(){
+   console.log("hello");
+
+   const getRandomInt = function(num) {
+    return Math.floor(Math.random() * num)
+  }
+
+  const getRandomElementFromArray = function(array) {
+    const randomInt = getRandomInt(array.length)
+    return array[randomInt]
+  }
+
+
+//    const images = ["leon2.jpeg", "captain.jpeg", "leon.jpeg"];
+   const $images = $(".home_image")
+
+   const imageSoup = function(){
+     const $singleImage = $(getRandomElementFromArray($images));
+    
+     const xpos = getRandomInt(window.innerWidth) 
+     const ypos = getRandomInt(window.innerHeight) 
+
+     $singleImage.css({
+        top: ypos + 100,
+        left: xpos - 200,
+
+     });
+
+     $('body').append($singleImage)
+
+     $singleImage.fadeIn(2000).fadeOut(2000, function () {
+        $(this).remove();
+     });
+   }; //end function imageSoup
+   
+
+   setInterval(imageSoup, 700);
+
+});
